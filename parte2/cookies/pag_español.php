b541ea1ce77d72e05bfd70e22cb30a41b1679d6f
Jorge Martinez <jlmartinezpinto@gmail.com>
2023-11-26 15:52:42 -0500

    todo Completo

---
+++ parte2/cookies/pag_español.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style/stylePag.css">
+</head>
+<body>
+    <div class="title">
+        <h1>EL BLOG DE LAS FLORES</h1>
+        </div>
+    <div class="container_topvar">
+        <nav>
+            <ul class="nav-bar">
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Inicio</a></li>
+                <li>
+                    <a href="#">Procesos</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Buscar productos</a></li>
+                        <li><a href="#">Agregar Productos</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="containerGreen">
+        <h2>¿Te gustan las flores mas que a nosotros?</h2>
+        <p>Las flores, con su belleza efímera y diversidad cautivadora, son auténticas joyas de la naturaleza. Estas estructuras biológicas especializadas, presentes en la mayoría de las plantas con flor, desempeñan roles cruciales en la reproducción y contribuyen a la increíble biodiversidad de nuestro planeta.</p>
+    </div>
+</body>
+</html>
---
+++ parte2/cookies/pag_ingles.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style/stylePag.css">
+</head>
+<body>
+    <div class="title">
+        <h1>The Flower Blog</h1>
+        </div>
+    <div class="container_topvar">
+        <nav>
+            <ul class="nav-bar">
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Home</a></li>
+                <li>
+                    <a href="#">Processes</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Search Products</a></li>
+                        <li><a href="#">Add Products</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="containerGreen">
+        <h2>¿Do you like flowers more than we do?</h2>
+        <p>Flowers, with their ephemeral beauty and captivating diversity, are authentic jewels of nature. These specialized biological structures, present in the majority of flowering plants, play crucial roles in reproduction and contribute to the incredible biodiversity of our planet.</p>
+    </div>
+</body>
+</html>
