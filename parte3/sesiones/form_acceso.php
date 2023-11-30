26a0680d1ff6cd119bcb84e5ec277cd0ea783137
Jorge <jlmartinezpinto@gmail.com>
2023-11-30 07:42:32 -0500

    correccion parte 1

---
+++ parte3/sesiones/form_acceso.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio Sesion</title>
+    <link rel="stylesheet" href="style/estilosform.css">
+</head>
+<body>
+<h4>ACCESO AL SISTEMA</h4>
+<div>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <label>Usuario:</label>
+        <input name="correo" type="email">
+        <label>Contraseña:</label>
+        <input name="password" type="password">
+        <input type="submit" name="enviar" value="ENVIAR">
+    </form>
+</div>
+</body>
+</html>
